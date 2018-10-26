e82074203a13ab9d6377dd51abd3ce0558c43e66
Krzysztof Michalak <krzysztof.michalak96@gmail.com>
2018-10-26 21:45:40 +0200

    Solve problem 18

---
+++ 018.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int digitsToInt(char a, char b) {
+  return (a - '0')*10 + (b - '0');
+}
+
+long max(long a, long b) {
+  if(a > b) return a;
+  else return b;
+}
+
+long sumMaxElements(int numbers[120], int position, int level) {
+  int leftChild = position + level;
+  int rightChild = position + level + 1;
+  if(rightChild >= 120) return 0;
+  return max(numbers[leftChild]+sumMaxElements(numbers, leftChild, level+1), numbers[rightChild]+sumMaxElements(numbers, rightChild, level+1));
+}
+
+void main() {
+  int numbers[120];
+  char input[360] = "75 95 64 17 47 82 18 35 87 10 20 04 82 47 65 19 01 23 75 03 34 88 02 77 73 07 63 67 99 65 04 28 06 16 70 92 41 41 26 56 83 40 80 70 33 41 48 72 33 47 32 37 16 94 29 53 71 44 65 25 43 91 52 97 51 14 70 11 33 28 77 73 17 78 39 68 17 57 91 71 52 38 17 14 91 43 58 50 27 29 48 63 66 04 68 89 53 67 30 73 16 69 87 40 31 04 62 98 27 23 09 70 98 73 93 38 53 60 04 23 ";
+
+  for(int i = 0; i<=359; i+=3) {
+    numbers[i/3] = digitsToInt(input[i], input[i+1]);
+  }
+
+  long result = numbers[0] + sumMaxElements(numbers, 0, 1);
+  printf("Result: %ld", result);
+}
